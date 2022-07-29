a01e59e093d614e1b7493407780e09a236658fb3
Aleksandr S <shurloff.a.s@gmail.com>
2022-07-29 20:15:39 +0300

    correction task 2 and add task 4

--- DZ_1/Zadacha_4/Program.cs
+++ DZ_1/Zadacha_4/Program.cs
@@ -1,18 +1,18 @@
-﻿int a, b;
+﻿int a, b, c;
 
 Console.Write("Enter number one: ");
 a = Convert.ToInt32(Console.ReadLine()); //Convert.ToInt32 - преобразует в int значение любого типа
 
 Console.Write("Enter number two: ");
-b = Convert.ToInt32(Console.ReadLine()); 
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter number three: ");
+c = Convert.ToInt32(Console.ReadLine());
+
 int max = a;
-int min = b;
 
 if (b > max) max = b;
-if (a < min) min = a;
+if (c > max) max = c;
 
 Console.Write("Max = ");
 Console.WriteLine(max);
-
-Console.Write("Min = ");
-Console.WriteLine(min);
