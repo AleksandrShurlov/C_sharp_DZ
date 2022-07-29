7bc81058c4fb5db7b722474837b464b8403f8f5c
Aleksandr S <shurloff.a.s@gmail.com>
2022-07-29 19:58:12 +0300

    add task 2

---
+++ DZ_1/Zadacha_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int a, b;
+
+Console.Write("Enter number one: ");
+a = Convert.ToInt32(Console.ReadLine()); //Convert.ToInt32 - преобразует в int значение любого типа
+
+Console.Write("Enter number two: ");
+b = Convert.ToInt32(Console.ReadLine()); 
+int max = a;
+int min = b;
+
+if (b > a) max = b;
+if (a < b) min = a;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
+
+Console.Write("Min = ");
+Console.WriteLine(min);
