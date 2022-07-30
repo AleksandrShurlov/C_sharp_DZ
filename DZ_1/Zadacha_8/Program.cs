341e1467ae07c46d298b87b63c09f085075cf24d
Aleksandr S <shurloff.a.s@gmail.com>
2022-07-30 19:11:40 +0300

    add task 8

---
+++ DZ_1/Zadacha_8/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+int b = 0;
+
+Console.Write("Enter a positive integer number: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+while(b < a - 1)
+{   
+    b = b + 2;
+    Console.WriteLine(b);
+}
+Console.Write("Even numbers from 1 to ");
+Console.Write(a);
