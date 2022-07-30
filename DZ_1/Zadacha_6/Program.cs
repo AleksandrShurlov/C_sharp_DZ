bf5e43f78124f8d2434d2981d49d270f6e5df25f
Aleksandr S <shurloff.a.s@gmail.com>
2022-07-30 18:23:07 +0300

    add task 6

---
+++ DZ_1/Zadacha_6/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+
+Console.Write("Enter number: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.Write("The number is even");
+}
+else
+{
+    Console.Write("The number is odd");
+}
