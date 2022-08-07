8167d89bf1defc2c4f422119d871a6a308e28041
Aleksandr S <shurloff.a.s@gmail.com>
2022-08-07 14:02:54 +0300

    add task 15, add second solution to task 10 and write comments on task 13

--- DZ_2/Zadacha_10/Program.cs
+++ DZ_2/Zadacha_10/Program.cs
@@ -1,10 +1,10 @@
-﻿void FillArray(int[] number1, int[] number2, int[] number3)
+﻿/*void FillArray(int[] number1, int[] number2, int[] number3) 
 {
     int length1 = number1.Length;
     int index1 = 0;
     while (index1 < length1)
     {
-        number1[index1] = new Random().Next(1, 9);
+        number1[index1] = new Random().Next(1, 10);
         index1++;
     }
 
@@ -12,7 +12,7 @@
     int index2 = 0;
     while (index2 < length2)
     {
-        number2[index2] = new Random().Next(0, 9);
+        number2[index2] = new Random().Next(0, 10);
         index2++;
     }
 
@@ -20,7 +20,7 @@
     int index3 = 0;
     while (index3 < length3)
     {
-        number3[index3] = new Random().Next(0, 9);
+        number3[index3] = new Random().Next(0, 10);
         index3++;
     }
 }
@@ -60,4 +60,16 @@ FillArray(array1, array2, array3);
 PrintArray(array1, array2, array3);
 
 Console.WriteLine();
-Console.WriteLine($"The second digit of this number: {array2[0]}");
+Console.WriteLine($"The second digit of this number: {array2[0]}");*/
+//Выше находится первый вариант решения, который мне изначально казался слишком сложным и, наверное, не очень правильным. Я долго думал над решением, реализовал вот так))
+//А ниже второй вариант, который пришёл мне в голову после решения 13 задачи, и я вернулся сюда, чтобы написать его.
+void Search2(int arg) 
+{   
+    arg = new Random().Next(100, 1000);
+    Console.WriteLine($"Number: {arg}");
+    Console.WriteLine($"The second digit of this number: {arg.ToString()[1]}");
+}
+
+int a = 0;
+
+Search2(a);
---
+++ DZ_2/Zadacha_15/Program.cs
@@ -0,0 +1,35 @@
+﻿void SearchWeekend(int[] number)
+{
+    int size = number.Length;
+    int index = 0;
+    int sat = 6;
+    int sun = 7;
+    while (index < size)
+    {
+        if(number[index] == sat || number[index] == sun)
+        {
+            Console.WriteLine($"{number[index]}: Wow, weekend");
+        }
+        else
+        {
+            Console.WriteLine($"{number[index]}: The Weekday"); 
+        }
+        index++;
+    }
+}
+
+void FillArray(int[] number)
+{
+    int length = number.Length;
+    int index = 0;
+    while (index < length)
+    {
+        number[index] = new Random().Next(1, 8);
+        index++;
+    }
+}
+
+int[] days = new int[1];
+
+FillArray(days);
+SearchWeekend(days);
