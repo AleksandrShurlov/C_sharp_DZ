d52f66d5fcb24c1dded0486166f7d09d4316efdf
Aleksandr S <shurloff.a.s@gmail.com>
2022-08-13 21:17:34 +0300

    add a second way to task 25

--- DZ_4/Zadacha_25/Program.cs
+++ DZ_4/Zadacha_25/Program.cs
@@ -1,15 +1,35 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) 
 // и возводит число A в натуральную степень B.
 
-void Exponent(int a, int b)
+/*void Exponent(int a, int b) // Первый способ
 {
+    Console.Write("Enter number A: ");
+    a = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter number B: ");
+    b = Convert.ToInt32(Console.ReadLine());
+    
     Console.Write($"A to the power of B is = {Math.Pow(a, b)}");
+}*/
+
+
+void Exponent(int a, int b) // Второй способ (цикл)
+{
+    Console.Write("Enter number A: ");
+    a = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter number B: ");
+    b = Convert.ToInt32(Console.ReadLine());
+
+    int result = 1;
+
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    Console.Write($"A to the power of B is = {result}");
 }
 
-Console.Write("Enter number A: ");
-int a = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Enter number B: ");
-int b = Convert.ToInt32(Console.ReadLine());
+int a = 0, b = 0;
 
 Exponent(a, b);
