fedbbea6bca0d64593dfb338a44af79b1b06651a
Aleksandr S <shurloff.a.s@gmail.com>
2022-08-13 20:44:47 +0300

    add task 27

---
+++ DZ_4/Zadacha_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+void SumDig(int number)
+{
+    Console.Write("Enter the number: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+    int remainder = 0;
+    while (number > 0)
+    {
+        remainder = number % 10;
+        number = number / 10;
+        sum = sum + remainder;
+    }
+    Console.WriteLine($"Sum of digits in number = {sum}");
+}
+
+int n = 0;
+
+SumDig(n);
