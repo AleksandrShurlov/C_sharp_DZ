29efb99dc9e54c1a0123a399e8e69fdba1e29f58
Aleksandr S <shurloff.a.s@gmail.com>
2022-08-15 20:59:56 +0300

    add task 36

--- DZ_5/Zadacha_36/Program.cs
+++ DZ_5/Zadacha_36/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void SumElementsOddPosition(int[] array)
+{   
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        sum = sum + array[i];
+    }
+    Console.WriteLine();
+    Console.WriteLine($"The sum of the elements in odd positions is {sum}");
+}
+
+int[] a = new int[6];
+
+FillArray(a);
+PrintArray(a);
+SumElementsOddPosition(a);
