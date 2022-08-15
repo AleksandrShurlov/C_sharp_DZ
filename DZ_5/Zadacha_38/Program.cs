1145a6ac88ae3a51312061701c5d6cd96dc0056b
Aleksandr S <shurloff.a.s@gmail.com>
2022-08-15 21:38:34 +0300

    add task 38

--- DZ_5/Zadacha_38/Program.cs
+++ DZ_5/Zadacha_38/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i = i + 2)
+    {
+        array[i] = new Random().Next(21, 101);
+    }
+
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        array[i] = new Random().Next(1, 21);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void DiffMinMax(int[] array)
+{
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < min) min = array[i];
+    }
+
+    int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max) max = array[i];
+    }
+
+    int diff = max - min;
+    Console.WriteLine();
+    Console.WriteLine($"The difference between the maximum and minimum element is {diff}");
+}
+
+int[] a = new int[6];
+
+FillArray(a);
+PrintArray(a);
+DiffMinMax(a);
