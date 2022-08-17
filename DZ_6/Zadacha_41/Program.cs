802458a778d03c6f2880b492be535afac3e10c69
Aleksandr S <shurloff.a.s@gmail.com>
2022-08-17 21:41:06 +0300

    add task 41

---
+++ DZ_6/Zadacha_41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void AmountMoreZero(int[] array)
+{   
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count = count + 1;
+        } 
+    }
+    Console.WriteLine($"User entered {count} numbers greater than zero");
+}
+
+int[] numbers;
+
+Console.Write("Enter numbers separated by spaces: ");
+numbers = Console.ReadLine().Split(' ')
+.Select(e => Convert.ToInt32(e)).ToArray();
+
+AmountMoreZero(numbers);
