96fa8ec733c12754ec561f6391ac9d53536c46be
Aleksandr S <shurloff.a.s@gmail.com>
2022-08-24 22:24:19 +0300

    add beta version task 52

--- DZ_7/Zadacha_50/Program.cs
+++ DZ_7/Zadacha_50/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает позиции элемента
+// либо значение элемента в двумерном массиве,
+// и возвращает значение либо индекс этого элемента или же указание,
+// что такого элемента нет.
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[4, 4];
+
+FillArray(matrix);
+PrintArray(matrix);
--- DZ_7/Zadacha_52/Program.cs
+++ DZ_7/Zadacha_52/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ArithmeticMeanColumn(int[,] matr)
+{
+    double sum1 = 0; double sum2 = 0; double sum3 = 0; double sum4 = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j == 0)
+            {
+                sum1 = sum1 + matr[i, j];
+            }
+            else if (j == 1)
+            {
+                sum2 = sum2 + matr[i, j];
+            }
+            else if (j == 2)
+            {
+                sum3 = sum3 + matr[i, j];
+            }
+            else if (j == 3)
+            {
+                sum4 = sum4 + matr[i, j];
+            }
+        }
+    }
+    double average1 = Math.Round(sum1 / matr.GetLength(0), 1);
+    double average2 = Math.Round(sum2 / matr.GetLength(0), 1);
+    double average3 = Math.Round(sum3 / matr.GetLength(0), 1);
+    double average4 = Math.Round(sum4 / matr.GetLength(0), 1);
+    Console.Write($"Arithmetic mean of each column: {average1}; {average2}; {average3}; {average4}.");
+}
+
+int[,] matrix = new int[3, 4];
+
+FillArray(matrix);
+PrintArray(matrix);
+ArithmeticMeanColumn(matrix);
