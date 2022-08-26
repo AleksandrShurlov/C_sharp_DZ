6f6bc46d269adcdf97d4694a00aac333b0355737
Aleksandr S <shurloff.a.s@gmail.com>
2022-08-26 23:18:34 +0300

    add  task 50

--- DZ_7/Zadacha_50/Program.cs
+++ DZ_7/Zadacha_50/Program.cs
@@ -26,7 +26,83 @@ void PrintArray(int[,] matr)
     }
 }
 
-int[,] matrix = new int[4, 4];
+void SearchIndex(int[,] matr, int find)
+{
+    int count = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (matr[i, j] == find)
+            {
+                Console.WriteLine($"The element exists and its index is {i}, {j}");
+                count += 1;
+            }
+        }
+    }
+    if (count == 0)
+    {
+        Console.WriteLine("There is no such number in the array.");
+    }
+}
 
-FillArray(matrix);
-PrintArray(matrix);
+void SearchElement(int[,] matr, int m, int n)
+{
+    int count = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (i == m && j == n)
+            {
+                Console.WriteLine($"Such an element exists and is equal to {matr[i, j]}");
+                count += 1;
+            }
+        }
+    }
+    if (count == 0)
+    {
+        Console.WriteLine("No such element.");
+    }
+}
+
+void SearchMethod()
+{
+    int[,] matrix = new int[4, 4];
+
+    Console.WriteLine("1. Find element by its index");
+    Console.WriteLine("2. Find element index");
+    Console.Write("Enter 1 or 2 to choose: ");
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    if (choice == 1)
+    {
+        Console.WriteLine("Enter element index [i, j].");
+
+        Console.Write("Enter i: ");
+        int i = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter j: ");
+        int j = Convert.ToInt32(Console.ReadLine());
+
+        FillArray(matrix);
+        PrintArray(matrix);
+        SearchElement(matrix, i, j);
+    }
+    else if (choice == 2)
+    {
+        Console.Write("Enter the element you are looking for: ");
+        int findElement = Convert.ToInt32(Console.ReadLine());
+
+        FillArray(matrix);
+        PrintArray(matrix);
+        SearchIndex(matrix, findElement);
+    }
+    else
+    {
+        Console.WriteLine("Incorrect values entered.");
+        return;
+    } 
+}
+
+SearchMethod();
