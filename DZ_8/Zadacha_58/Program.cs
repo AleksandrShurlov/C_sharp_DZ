f87648cbe8ba905e7f58d06e5ec3f8329d29b589
Aleksandr S <shurloff.a.s@gmail.com>
2022-08-31 22:53:08 +0300

    add  task 58

--- DZ_8/Zadacha_58/Program.cs
+++ DZ_8/Zadacha_58/Program.cs
@@ -1,6 +1,5 @@
-﻿
-
-
+﻿// Задайте две матрицы. Напишите программу,
+// которая будет находить произведение двух матриц
 
 void FillArray(int[,] matr)
 {
@@ -24,3 +23,43 @@ void PrintArray(int[,] matr)
         Console.WriteLine();
     }
 }
+
+void MatrixProduct(int[,] matr1, int[,] matr2, int[,] resultMatr)
+{
+    for (int i = 0; i < resultMatr.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatr.GetLength(1); j++)
+        {
+            resultMatr[i, j] = matr1[i, j] * matr2[i, j];
+        }
+    }
+}
+
+void StartMethod()
+{
+    Console.WriteLine("Enter the size of the array MxN.");
+
+    Console.Write("Enter m: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    int[,] matrix1 = new int[m, n];
+    int[,] matrix2 = new int[m, n];
+    int[,] resultMatrix = new int[m, n];
+
+    Console.WriteLine();
+    FillArray(matrix1);
+    PrintArray(matrix1);
+    Console.WriteLine();
+
+    FillArray(matrix2);
+    PrintArray(matrix2);
+    Console.WriteLine();
+
+    MatrixProduct(matrix1, matrix2, resultMatrix);
+    PrintArray(resultMatrix);
+}
+
+StartMethod();
